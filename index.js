1dfea6b9972834574223a6cee29767ec71990177
archana-acharya1 <145975347+archana-acharya1@users.noreply.github.com>
2025-09-10 15:41:21 +0545

    abc

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 import "dotenv/config";
 import express from "express";
 import authRouter from "./auth_Router.js";
+import connectDB from "./db/connect.js";
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/auth", authRouter);
 
 // test route:
 app.get("/", (req, res) => {
-    res.send("Test Route is fine!");
+  res.send("Test Route is fine!");
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error("[ERROR]", err);
-    res.status(500).json({
-        error: err.message || "Internal Server Error"
-    });
+  console.error("[ERROR]", err);
+  res.status(500).json({
+    error: err.message || "Internal Server Error",
+  });
 });
 
-const port = process.env.PORT || 5555;
+// const port = process.env.PORT || 5555;
+
+// const start = async () => {
+//     try {
+//         app.listen(port, () => {
+//             console.log(`Server is running on port: ${port}`);
+//         });
+//     } catch (err) {
+//         console.log("[SERVER ERROR]", err);
+//     }
+// };
+
+// start();
+
+// defing the port and ready for the server to start..
+const port = process.env.PORT || 3000;
 
 const start = async () => {
-    try {
-        app.listen(port, () => {
-            console.log(`Server is running on port: ${port}`);
-        });
-    } catch (err) {
-        console.log("[SERVER ERROR]", err);
-    }
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is running at ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 start();
